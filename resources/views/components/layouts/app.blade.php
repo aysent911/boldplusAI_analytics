e972df89e58b6566329c91796a91e8b05319459b
Teddy Silvance <aysent911@gmail.com>
2025-08-26 17:53:55 +0300

    added layout page

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,8 +5,56 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>{{ $title ?? 'Page Title' }}</title>
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
+        @vite('resources/css/app.css')
+        @fluxAppearance
     </head>
-    <body>
-        {{ $slot }}
+    <body class="min-h-screen bg-white dark:bg-zinc-800">
+        <flux:sidebar sticky stashable class="bg-zinc-50 dark:bg-zinc-900 border-r rtl:border-r-0 rtl:border-l border-zinc-200 dark:border-zinc-700">
+            <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
+            <flux:brand href="#" logo="images/logo.png" name="Org Inc." class="px-2 dark:hidden" />
+            <flux:brand href="#" logo="images/logo.png" name="Org Inc." class="px-2 hidden dark:flex" />
+            <flux:input as="button" variant="filled" placeholder="Search..." icon="magnifying-glass" />
+            <flux:navlist variant="outline">
+                <flux:navlist.item icon="home" href="#" current>Home</flux:navlist.item>
+                <flux:navlist.item icon="inbox" badge="12" href="#">Inbox</flux:navlist.item>
+                <flux:navlist.item icon="document-text" href="#">Documents</flux:navlist.item>
+                <flux:navlist.item icon="calendar" href="#">Calendar</flux:navlist.item>
+                <flux:navlist.group expandable heading="Favorites" class="hidden lg:grid">
+                    <flux:navlist.item href="#">Marketing site</flux:navlist.item>
+                    <flux:navlist.item href="#">Android app</flux:navlist.item>
+                    <flux:navlist.item href="#">Brand guidelines</flux:navlist.item>
+                </flux:navlist.group>
+            </flux:navlist>
+            <flux:spacer />
+            <flux:navlist variant="outline">
+                <flux:navlist.item icon="cog-6-tooth" href="#">Settings</flux:navlist.item>
+                <flux:navlist.item icon="information-circle" href="#">Help</flux:navlist.item>
+            </flux:navlist>
+            <flux:dropdown position="top" align="start" class="max-lg:hidden">
+                <flux:profile avatar="images/logo.png" name="Olivia Martin" />
+                <flux:menu>
+                    <flux:menu.radio.group>
+                        <flux:menu.radio checked>Analytics</flux:menu.radio>
+                        <flux:menu.radio>boldplusAI</flux:menu.radio>
+                    </flux:menu.radio.group>
+                    <flux:menu.separator />
+                    <flux:menu.item icon="arrow-right-start-on-rectangle">Logout</flux:menu.item>
+                </flux:menu>
+            </flux:dropdown>
+        </flux:sidebar>
+        <flux:header class="block! bg-white lg:bg-zinc-50 dark:bg-zinc-900 border-b border-zinc-200 dark:border-zinc-700">
+            <flux:navbar scrollable>
+                <flux:navbar.item href="#" current>Dashboard</flux:navbar.item>
+                <flux:navbar.item badge="32" href="#">Orders</flux:navbar.item>
+                <flux:navbar.item href="#">Catalog</flux:navbar.item>
+                <flux:navbar.item href="#">Configuration</flux:navbar.item>
+            </flux:navbar>
+        </flux:header>
+        <flux:main>
+            {{ $slot }}
+        </flux:main>
+        @fluxScripts
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
+use App\Livewire\Home;
 use App\Livewire\Counter;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', [HomeController::class, 'home']);
+Route::get('/home', Home::class);
 Route::get('/counter', Counter::class);
